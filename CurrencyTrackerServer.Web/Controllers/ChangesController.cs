abdec3fbc77d6b053af019e8ab2435ce58acbf69
dpvnbd <dpvnbd@gmail.com>
2017-09-12 18:04:04 +0300

    Configure DB connection and dependencies of services to inject

--- CurrencyTrackerServer.Web/Controllers/ChangesController.cs
+++ CurrencyTrackerServer.Web/Controllers/ChangesController.cs
@@ -1,7 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CurrencyTrackerServer.ChangeTrackerService.Concrete.ProviderSpecific.Bittrex;
+using CurrencyTrackerServer.ChangeTrackerService.Concrete.ProviderSpecific.Poloniex;
+using CurrencyTrackerServer.ChangeTrackerService.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,36 +14,19 @@ namespace CurrencyTrackerServer.Web.Controllers
     [Route("api/[controller]")]
     public class ChangesController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+      private readonly BittrexTimerWorker _bWorker;
+      private readonly PoloniexTimerWorker _pWorker;
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+      public ChangesController(BittrexTimerWorker bWorker, PoloniexTimerWorker pWorker)
+      {
+        _bWorker = bWorker;
+        _pWorker = pWorker;
+      }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+      [HttpGet()]
+      public IEnumerable<Change> Get()
+      {
+        return _bWorker.Monitor.GetHistory();
+      }
     }
 }
