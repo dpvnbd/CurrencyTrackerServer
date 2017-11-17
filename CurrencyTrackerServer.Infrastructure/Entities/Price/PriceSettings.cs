709d99799ecba992829a94cb794d94f8e408e30c
dpvnbd <dpvnbd@gmail.com>
2017-11-17 16:45:56 +0200

    Allow empty and whitespace emails in price settings

--- CurrencyTrackerServer.Infrastructure/Entities/Price/PriceSettings.cs
+++ CurrencyTrackerServer.Infrastructure/Entities/Price/PriceSettings.cs
@@ -8,17 +8,22 @@ namespace CurrencyTrackerServer.Infrastructure.Entities.Price
   public class PriceSettings
   {
     private List<Price> _currencies;
+    private string _email;
 
 
     public List<Price> Prices
     {
       get => _currencies ?? (_currencies = new List<Price>());
-      set => _currencies = value; 
+      set => _currencies = value;
     }
 
     public bool SendNotifications { get; set; }
 
     [EmailAddress]
-    public string Email { get; set; }
+    public string Email
+    {
+      get => _email;
+      set => _email = string.IsNullOrWhiteSpace(value) ? null : value; //allow empty or whitespace emails
+    }
   }
 }
